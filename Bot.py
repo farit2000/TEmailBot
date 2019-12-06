4c7987c56e301267a0dda6c04adebc3b45c7ebdb
Farit Shamardanov <farit.shamardanov@outlook.com>
2019-12-06 23:53:13 +0300

    Try add work with telegram bot

--- Bot.py
+++ Bot.py
@@ -41,8 +41,8 @@ def reply_to_message(message):
 # SERVER SIDE
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
-    # global json_str
-    # json_str = str(request.stream.read().decode('utf-8'))
+    global json_str
+    json_str = request.stream.read().decode('utf-8')
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
